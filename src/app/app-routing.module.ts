18e890a8f40f5dcb2bcbd4e6f50a1a9ad6c43ed8
abhxi-r-s <abhishekunni8281@gmail.com>
2022-07-15 14:40:37 +0530

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddcourseComponent } from './addcourse/addcourse.component';
 import { ViewcourseComponent } from './viewcourse/viewcourse.component';
 
-const routes: Routes = [{path:"",component:ViewcourseComponent},{path:"",component:AddcourseComponent}];
+const routes: Routes = [{path:"",component:ViewcourseComponent},{path:"addcourse",component:AddcourseComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
